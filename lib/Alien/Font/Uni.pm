3413155375e2ffa204bb2db0836eb1e224fecc4e
Lichtkind <lichtkind@cpan.org>
2022-11-16 14:47:56 +0100

    prepare first release

--- lib/Alien/Font/Uni.pm
+++ lib/Alien/Font/Uni.pm
@@ -1,15 +1,56 @@
 
 package Alien::Font::Uni;
+our $VERSION = '0.0_1';
 
 use File::ShareDir 'dist_dir';
 use File::Spec;
  
-# Where are distribution-level shared data files kept
+sub get_path { File::Spec->catfile( dist_dir(__PACKAGE__) , 'unifont-'.font_version().'.ttf') }
 
-
-sub get_path {
-    File::Spec->catfile( dist_dir('Alien-Font-Uni') , 'c');
-}
+sub font_version { '15.0.01' }
+sub font_vstring { v15.0.01  }
 
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Alien-Font-Uni - providing Unifont (file or location)
+
+=head1 SYNOPSIS 
+
+    use Alien::Font::Uni;
+    my $path = Alien::Font::Uni::get_path();
+    my $vstring = Alien::Font::Uni::font_vstring();
+    my $string  = Alien::Font::Uni::font_version();
+
+=head1 DESCRIPTION
+
+This module was created as an optional dependency for L<Chart> to have an unicode complete
+scaleable font file. Thus only the .ttf file is provided.
+Distros might redirect to pre-existing resources.
+
+=head1 FONT
+
+Unifont 15, Copyright(c) 1998 - 2022 by	Roman Czyborra, Paul Hardy
+and contributors. Licensed under OFL 1.1
+
+=head1 AUTHOR
+
+Herbert Breunung (lichtkind@cpan.org)
+
+=back
+
+=head1 COPYRIGHT
+
+Copyright(c) 2022 by Herbert Breunung
+
+All rights reserved.  This program is free software; you can
+redistribute it and/or modify it under the GPL version 3.
+
+=cut
+
